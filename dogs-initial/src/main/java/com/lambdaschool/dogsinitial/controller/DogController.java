316edd5ed920f3ddb03361c89c84f22c756ec357
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-05 13:46:34 -0700

    Initial Commit, replaced files with the ones from yesterday???

--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/controller/DogController.java
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/controller/DogController.java
@@ -1,11 +1,15 @@
-package com.lambdaschool.dogsinitial;
+package com.lambdaschool.dogsinitial.controller;
 
+import com.lambdaschool.dogsinitial.exception.ResourceNotFoundException;
+import com.lambdaschool.dogsinitial.model.Dog;
+import com.lambdaschool.dogsinitial.DogsinitialApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 
@@ -24,7 +28,12 @@ public class DogController
     @GetMapping(value = "/{id}")
     public ResponseEntity<?> getDogDetail(@PathVariable long id)
     {
-        Dog rtnDog = DogsinitialApplication.ourDogList.findDog(d -> (d.getId() == id));
+        Dog rtnDog;
+        if(DogsinitialApplication.ourDogList.findDog(d -> (d.getId() == id))== null){
+            throw new ResourceNotFoundException("The dog with the id "+ id + " does not exist");
+        } else{
+            rtnDog = DogsinitialApplication.ourDogList.findDog(d -> (d.getId() == id));
+        }
         return new ResponseEntity<>(rtnDog, HttpStatus.OK);
     }
 
@@ -35,5 +44,14 @@ public class DogController
         ArrayList<Dog> rtnDogs = DogsinitialApplication.ourDogList.
                 findDogs(d -> d.getBreed().toUpperCase().equals(breed.toUpperCase()));
         return new ResponseEntity<>(rtnDogs, HttpStatus.OK);
+    }
+
+    // localhost:2019/dogs/dogtable
+    @GetMapping(value = "/dogtable")
+    public ModelAndView showDogTable(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("dogs");
+        mav.addObject("dogList", DogsinitialApplication.ourDogList.dogList);
+        return mav;
     }
 }
